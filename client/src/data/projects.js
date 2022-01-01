8fdd24205f61f207e17be88cf3bbddad5b8d44fb
the-coding-pie <thecodingpie@gmail.com>
2022-01-01 18:52:12 +0530

    add project

---
+++ client/src/data/projects.js
@@ -0,0 +1,25 @@
+export const projects = [
+  {
+    id: 1,
+    name: "Google",
+    icon: null,
+    boards: [
+      {
+        id: 1,
+        name: "Board 1",
+        color: "#00C2E2",
+      },
+      {
+        id: 2,
+        name: "Board 2",
+        color: "#5DBF48",
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: "Newman",
+    icon: null,
+    boards: [],
+  },
+];
