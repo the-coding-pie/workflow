a960af78dde6fccf5bb3837122c66d88fb625428
the-coding-pie <thecodingpie@gmail.com>
2022-01-14 17:05:51 +0530

    new

--- client/src/components/Modal/Modal.tsx
+++ client/src/components/Modal/Modal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { MdClose } from "react-icons/md";
 import { useDispatch } from "react-redux";
 import useEscClose from "../../hooks/useEscClose";
@@ -35,40 +35,54 @@ const Modal = ({
       Component = null;
   }
 
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+
+    return () => {
+      document.body.style.overflowX = "hidden";
+      document.body.style.overflowY = "auto";
+    };
+  }, []);
+
   return (
     <div
-      className="backdrop fixed top-0 bottom-0 left-0 right-0 z-50 flex items-center justify-center w-full h-full overflow-y-auto bg-black bg-opacity-60"
+      className="backdrop fixed top-0 bottom-0 left-0 right-0 z-50 flex items-center justify-center w-full h-full overflow-y-auto m-auto bg-black bg-opacity-60"
       style={{
         maxHeight: "100vh",
       }}
     >
-      <div
-        className={`relative rounded modal flex flex-col `}
-        style={{
-          minWidth: "34rem",
-          background: bgColor ? bgColor : "#ffffff",
-          color: textColor ? textColor : "inherit",
-        }}
-        ref={ref}
-      >
-        {showCloseBtn === true && (
-          <button
-            onClick={handleClose}
-            type="button"
-            className="modal__close-btn absolute right-2 z-10 top-2"
-          >
-            <MdClose color={textColor ? textColor : "inherit"} size={21} />
-          </button>
-        )}
-        {modalTitle && (
-          <div className="modal__title">
-            <h3 className="text-xl font-semibold p-2">{modalTitle}</h3>
-          </div>
-        )}
+      <div className="absolute top-16 flex flex-col items-center">
+        <div
+          className={`relative rounded modal flex flex-col`}
+          style={{
+            minWidth: "34rem",
+            maxWidth: "80%",
+            background: bgColor ? bgColor : "#ffffff",
+            color: textColor ? textColor : "inherit",
+          }}
+          ref={ref}
+        >
+          {showCloseBtn === true && (
+            <button
+              onClick={handleClose}
+              type="button"
+              className="modal__close-btn absolute right-2 z-10 top-2"
+            >
+              <MdClose color={textColor ? textColor : "inherit"} size={21} />
+            </button>
+          )}
+          {modalTitle && (
+            <div className="modal__title">
+              <h3 className="text-xl font-semibold p-2">{modalTitle}</h3>
+            </div>
+          )}
 
-        {Component !== null && <Component {...{ ...modalProps }} />}
+          <div className={`${showCloseBtn ? "mr-4" : ""}`}>
+            {Component !== null && <Component {...{ ...modalProps }} />}
+          </div>
+        </div>
+        <div className="empty-space pb-14"></div>
       </div>
-      <div className="empty-space pb-14"></div>
     </div>
   );
 };
