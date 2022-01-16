c91d86ebe4eb9f2866049064a987cbbdbbc26a7d
the-coding-pie <thecodingpie@gmail.com>
2022-01-16 19:46:55 +0530

    new

--- client/src/components/Sidebar/Sidebar.tsx
+++ client/src/components/Sidebar/Sidebar.tsx
@@ -65,7 +65,7 @@ const Sidebar = () => {
   return (
     <aside
       className={`sidebar z-20 shadow-xl fixed left-0 top-0 bottom-0 overflow-x-hidden box-border ${
-        show ? "w-60" : "w-0"
+        show ? "w-60" : "hidden"
       } bg-white h-screen flex flex-col transition-all`}
     >
       <header className="h-14 flex justify-between items-center px-4 border-b">
