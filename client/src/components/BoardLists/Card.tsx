9321de2f7ad2e7abafb7af05afa8bbd7c80ee8f8
the-coding-pie <thecodingpie@gmail.com>
2022-02-03 12:39:31 +0530

    new

--- client/src/components/BoardLists/Card.tsx
+++ client/src/components/BoardLists/Card.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Card = () => {
   return (
-    <li>
+    <li className="bg-white mb-2 rounded p-2 shadow hover:bg-slate-100 cursor-pointer font-normal text-gray-900">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Est vero facilis
       beatae, vel laboriosam accusantium quo natus voluptas distinctio illum!
       Rerum, tempore odio error ad laborum nulla aut sed voluptatibus.
--- client/src/components/BoardLists/List.tsx
+++ client/src/components/BoardLists/List.tsx
@@ -1,32 +1,32 @@
 import React from "react";
+import { HiOutlinePlus } from "react-icons/hi";
 import Card from "./Card";
 
 const List = () => {
   return (
     <div
-      className="list first:ml-4 bg-slate-100 shadow p-2 rounded flex flex-col"
+      className="list first:ml-4 p-2 rounded flex flex-col h-min"
       style={{
+        background: "#EBECF0",
         flex: "0 0 18rem",
         maxHeight: "calc(100vh - 10.2rem)",
       }}
     >
       <header className="list__header mb-1">
-        <h3 className="list-title font-semibold text-base">Hello</h3>
+        <h3 className="list-title font-semibold text-base px-2">Hello</h3>
       </header>
 
       <ul
         id="list-items"
-        className="list-items flex-1 flex flex-col overflow-y-auto"
+        className="list-items flex-1 flex flex-col overflow-y-auto pr-1"
       >
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+       
       </ul>
 
-      <button>Add a card</button>
+      <button className="w-full cursor-pointer flex items-center px-2 py-1.5 hover:bg-gray-300 rounded text-gray-700 hover:text-gray-900 mt-1">
+        <HiOutlinePlus className="mr-1" size={18} />
+        <span>Add a card</span>
+      </button>
     </div>
   );
 };
