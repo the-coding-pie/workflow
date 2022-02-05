e0fd7c19ad92a7dec189799f71c55f25e8306674
the-coding-pie <thecodingpie@gmail.com>
2022-02-05 15:42:37 +0530

    enw'

--- client/src/components/BoardMembers/BoardMembers.tsx
+++ client/src/components/BoardMembers/BoardMembers.tsx
@@ -18,16 +18,32 @@ const BoardMembers = ({ boardId, spaceId, role, members }: Props) => {
 
   return (
     <div className="board-members flex items-center">
-      {members.map((m: BoardMemberObj) => (
-        <BoardMember
-          key={m._id}
-          member={m}
-          boardId={boardId}
-          boardAdmins={boardAdmins}
-          myRole={role}
-          spaceId={spaceId}
-        />
-      ))}
+      {members.length > 8 ? (
+        <div className="flex items-center">
+          {members.slice(0, 8).map((m: BoardMemberObj) => (
+            <BoardMember
+              key={m._id}
+              member={m}
+              boardId={boardId}
+              boardAdmins={boardAdmins}
+              myRole={role}
+              spaceId={spaceId}
+            />
+          ))}{" "}
+          <div className="w-8 h-8 rounded-full bg-white flex items-center justify-center text-sm font-medium">+{members.slice(8).length}</div>
+        </div>
+      ) : (
+        members.map((m: BoardMemberObj) => (
+          <BoardMember
+            key={m._id}
+            member={m}
+            boardId={boardId}
+            boardAdmins={boardAdmins}
+            myRole={role}
+            spaceId={spaceId}
+          />
+        ))
+      )}
     </div>
   );
 };
